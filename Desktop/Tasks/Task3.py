149583b1f6619019a68e884d1976ad77fdd06219
nishitavemula14 <nishitavemula14@users.noreply.github.com>
2026-05-25 14:07:53 +0530

    Add Task3.py

---
+++ Desktop/Tasks/Task3.py
@@ -0,0 +1,10 @@
+N = int(input("enter a number:"))
+
+for num in range(2, N+1):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
